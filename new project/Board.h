ed9fde20e9bc04e0a0ec22712f59e4693e319f22
Muhammad Zair <mhammadzair@gmail.com>
2024-04-17 13:21:23 +0500

    completed initialization work in player and match class by zair

--- new project/Board.h
+++ new project/Board.h
@@ -8,5 +8,11 @@ class Board {
 	int cols;
 	vector<Track> Tracks;
 public:
+	Board();
+	Board(int rows,int cols,vector<Track> Tracks) {
+		this->cols = cols;
+		this->rows = rows;
+		this->Tracks = Tracks;
+	}
 	void display_board();
 };
